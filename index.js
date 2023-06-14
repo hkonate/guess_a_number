9020783248f7aa8bb0a86d68604cb27b70f62cb0
hkonate <konate.tronca@gmail.com>
2023-06-14 14:54:53 +0200

    step 2 done

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 function askNumber() {
-  let givenNumber = prompt("Hi, can you give me a number please");
+  return prompt("Hi, can you give me a number please");
+}
+function didIWin(givenNumber) {
+  if (parseInt(givenNumber) === 22) {
+    alert("Bravo ! Vous avez deviné le nombre");
+  } else if (parseInt(givenNumber) > 22) {
+    alert("Plus grand");
+  } else if (parseInt(givenNumber) < 22) {
+    alert("Plus petit");
+  } else {
+    alert("Erreur de format (1, 10, 25, 68)");
+  }
 }
 
-askNumber();
+function gameplay() {
+  let givenNumber;
+  do {
+    givenNumber = askNumber();
+    didIWin(givenNumber);
+  } while (givenNumber !== 22 || isNaN(parseInt(givenNumber)));
+}
+
+gameplay();
