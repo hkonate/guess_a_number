8953b3478202739078a79781c4bb6dda8bff448a
hkonate <konate.tronca@gmail.com>
2023-06-15 01:56:12 +0200

    step 5 done

--- index.js
+++ index.js
@@ -1,36 +1,68 @@
 function askNumber() {
   return prompt("Donnez-moi un nombre entier");
 }
+// function didIWin(givenNumber, numbertoFind) {
+//   if (isNaN(parseInt(givenNumber))) {
+//     alert(
+//       "Erreur de format, accepte seulement les nombres entiers exemple de format accepté (1, 10, 25, 68)"
+//     );
+//     console.log(Number.isInteger(parseInt(givenNumber)));
+//     return false;
+//   } else if (parseInt(givenNumber) > numbertoFind) {
+//     alert("Plus grand");
+//     return false;
+//   } else if (parseInt(givenNumber) < numbertoFind) {
+//     alert("Plus petit");
+//     return false;
+//   } else {
+//     alert("Bravo ! Vous avez deviné le nombre");
+//     return true;
+//   }
+// }
 function didIWin(givenNumber, numbertoFind) {
   if (isNaN(parseInt(givenNumber))) {
     alert(
       "Erreur de format, accepte seulement les nombres entiers exemple de format accepté (1, 10, 25, 68)"
     );
-    console.log(Number.isInteger(parseInt(givenNumber)));
-    return false;
-  } else if (parseInt(givenNumber) > numbertoFind) {
-    alert("Plus grand");
     return false;
   } else if (parseInt(givenNumber) < numbertoFind) {
+    alert("Plus grand");
+    return false;
+  } else if (parseInt(givenNumber) > numbertoFind) {
     alert("Plus petit");
     return false;
   } else {
-    alert("Bravo ! Vous avez deviné le nombre");
+    document.querySelector("#box").innerText =
+      "Bravo ! Vous avez deviné le nombre";
     return true;
   }
 }
 let numbertoFind = -1;
-
+let count = 0;
 function askNumberToFind() {
-  numbertoFind = prompt("Donnez-moi un nombre à deviner, s'il vous plait");
-  if (parseInt(numbertoFind) < 0 || parseInt(numbertoFind) > 50)
+  numbertoFind = prompt(
+    "Donnez-moi un nombre à deviner entre 0 et 50, s'il vous plait"
+  );
+  if (
+    parseInt(numbertoFind) < 0 ||
+    parseInt(numbertoFind) > 50 ||
+    !numbertoFind
+  )
     askNumberToFind();
 }
 
-function gameplay() {
-  const givenNumber = askNumber();
-  const result = didIWin(givenNumber, numbertoFind);
-  if (!result) gameplay();
-}
+// function gameplay() {
+//   // const givenNumber = askNumber();
+//   // if (!result) gameplay();
+//   console.log(numbertoFind);
+// }
+// gameplay();
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  didIWin(document.querySelector("input").value, numbertoFind);
+  count++;
+  document.querySelector(
+    "#try"
+  ).innerText = `Votre nombre de tentatives est de ${count}`;
+});
 askNumberToFind();
-gameplay();
