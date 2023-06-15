8d5590656e21c1165742d51e8f3fc9b14870738c
hkonate <konate.tronca@gmail.com>
2023-06-15 10:45:17 +0200

    step 6 done

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let numbertoFind = -1;
+let count = 0;
+let mininal = 0;
+let maximal = 50;
 function askNumber() {
   return prompt("Donnez-moi un nombre entier");
 }
@@ -25,11 +29,23 @@ function didIWin(givenNumber, numbertoFind) {
       "Erreur de format, accepte seulement les nombres entiers exemple de format accepté (1, 10, 25, 68)"
     );
     return false;
-  } else if (parseInt(givenNumber) < numbertoFind) {
-    alert("Plus grand");
+  } else if (parseInt(givenNumber) < parseInt(numbertoFind)) {
+    if (parseInt(givenNumber) < mininal)
+      alert("Le nombre proposé n'est pas dans la range");
+    else if (parseInt(givenNumber) > mininal) {
+      mininal = parseInt(givenNumber);
+      document.querySelector("#min").innerText = `${mininal} >`;
+      alert("Plus grand");
+    }
     return false;
-  } else if (parseInt(givenNumber) > numbertoFind) {
-    alert("Plus petit");
+  } else if (parseInt(givenNumber) > parseInt(numbertoFind)) {
+    if (parseInt(givenNumber) > maximal)
+      alert("Le nombre proposé n'est pas dans la range");
+    else if (parseInt(givenNumber) < maximal) {
+      maximal = parseInt(givenNumber);
+      document.querySelector("#max").innerText = `< ${maximal}`;
+      alert("Plus petit");
+    }
     return false;
   } else {
     document.querySelector("#box").innerText =
@@ -37,8 +53,6 @@ function didIWin(givenNumber, numbertoFind) {
     return true;
   }
 }
-let numbertoFind = -1;
-let count = 0;
 function askNumberToFind() {
   numbertoFind = prompt(
     "Donnez-moi un nombre à deviner entre 0 et 50, s'il vous plait"
